16962a2516075a0210811adcac7623ee102b1f01
KeithKida <62222088+LimitedFlames@users.noreply.github.com>
2025-10-08 16:47:44 +0000

    Co-authored-by: Wade Xu <Neko12138@users.noreply.github.com> Co-authored-by: JM704-m <JM704-m@users.noreply.github.com>

--- src/main.ts
+++ src/main.ts
@@ -4,3 +4,23 @@ import "./style.css";
 document.body.innerHTML = `
   <p>Example image asset: <img src="${exampleIconUrl}" class="icon" /></p>
 `;
+
+const counterDiv = document.createElement("div");
+counterDiv.textContent = "0 🐱";
+counterDiv.className = "counter-display";
+document.body.appendChild(counterDiv);
+
+// -make add burger button
+const clickButton = document.createElement("button");
+clickButton.textContent = "🐱";
+clickButton.className = "emoji-button";
+document.body.appendChild(clickButton);
+
+console.log("never gonna give you up");
+
+document.addEventListener("DOMContentLoaded", () => {
+  const butn: HTMLButtonElement = document.createElement("button");
+  butn.type = "button";
+  butn.innerHTML = "😟";
+  document.body.append(butn);
+});
